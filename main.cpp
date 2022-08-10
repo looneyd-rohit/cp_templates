757735b094e6b37a99887851c608eb290598812b
Rohit Sadhu <rohitsadhupersonal@gmail.com>
2022-08-10 22:53:16 +0530

    going well

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 const int M = 1e9+7;
 const int s = 1e5+1;
+// #define		int 		   long long
 #define		INF 		   1e9+69
 #define 	ull 		   unsigned long long
 #define     ll             long long
@@ -9,99 +10,37 @@ const int s = 1e5+1;
 #define     nsync          ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define     t()            int t; cin>>t; while(t--)
 
-class STRSQLazy{
-private:
+vector<int> parent;
+vector<int> sizeArr;
+set<int> sizes;
+
+void makeSet(int n){
+	for(int i=1; i<=n; i++){
+		parent[i] = i;
+		sizeArr[i] = 1;
+		// sizes.insert(1);
+	}
+}
+
+int findParent(int u){
+	if(u == parent[u]) return u;
+	return parent[u] = findParent(parent[u]);
+}
+
+void merge(int u, int v){
+	sizes.erase(sizes.find(sizeArr[u]));
+	sizes.erase(sizes.find(sizeArr[v]));
+	sizes.insert(sizeArr[u] + sizeArr[v]);
+}
+
+void unionSet(int u, int v){
+	u = findParent(u);
+	v = findParent(v);
 	
-public:
-	vector<int> segArr, lazy;
-	STRSQLazy(){
-		segArr.resize(1e5+1, 0);
-		lazy.resize(1e5+1, 0);
-	}
-	STRSQLazy(int n){
-		segArr.resize(4 * n + 1, 0);
-		lazy.resize(4 * n + 1, 0);
-	}
-	
-	void build(int index, int low, int high, vector<int>& arr){
-		if(low==high){
-			segArr[index] = arr[low];
-			return;
-		}
-		
-		int mid = low + ((high - low ) >> 1);
-		
-		build(2*index+1, low, mid, arr);
-		build(2*index+2, mid+1, high, arr);
-		segArr[index] = segArr[2*index+1] + segArr[2*index+2];
-	}
-	
-	int rangeQuery(int index, int low, int high, int l, int h){
-		// if previous updates are pending just complete them
-		if(lazy[index]!=0){
-			segArr[index] += (high - low + 1) * lazy[index];
-			
-			// if there exists child nodes then propagate
-			// the updates
-			if(low!=high){
-				lazy[2*index+1] += lazy[index];
-				lazy[2*index+2] += lazy[index];
-			}
-			
-			lazy[index] = 0;
-		}
-		
-		// if no overlap
-		if(h < low || l > high){ return 0; }
-		
-		// if complete overlap --> l <= low <= high <= h
-		if(l <= low and high <= h){
-			return segArr[index];
-		}
-		
-		int mid = low + ((high - low ) >> 1);
-		int left = rangeQuery(2*index+1, low, mid, l, h);
-		int right = rangeQuery(2*index+2, mid+1, high, l, h);
-		return (left+right);
-	}
-	
-	void rangeUpdate(int index, int low, int high, int l, int h, int val){
-		// if previous updates are pending just complete them
-		if(lazy[index]!=0){
-			segArr[index] += (high - low + 1) * lazy[index];
-			
-			// if there exists child nodes then propagate
-			// the updates
-			if(low!=high){
-				lazy[2*index+1] += lazy[index];
-				lazy[2*index+2] += lazy[index];
-			}
-			
-			lazy[index] = 0;
-		}
-		
-		// no overlap
-		if(h < low || l > high){ return; }
-		
-		// complete overlap
-		if(l <= low and high <= h){
-			segArr[index] += (high - low + 1)*val;
-			
-			// propagate the update to the child
-			if(low!=high){
-				lazy[2*index+1] += val;
-				lazy[2*index+2] += val;
-			}
-			return;
-		}
-		
-		// partial overlap
-		int mid = low + ((high - low ) >> 1);
-		rangeUpdate(2*index+1, low, mid, l, h, val);
-		rangeUpdate(2*index+2, mid+1, high, l, h, val);
-		segArr[index] = segArr[2*index+1] + segArr[2*index+2];
-	}
-};
+	parent[v] = u;
+	merge(u, v);
+	sizeArr[u] += sizeArr[v];
+}
 
 int32_t main(){
     nsync;
@@ -110,37 +49,33 @@ int32_t main(){
     freopen("output.txt", "w", stdout);
     freopen("error.txt", "w", stderr);
     #endif
-    // t(){
-    	
-    	
-    // }
+    // t(){ }
+    
     int n;
     cin>>n;
-    vector<int> arr(n, 0);
-    for(int i=0; i<n; i++) cin>>arr[i];
-    // build the segment tree
-    STRSQLazy seg(n);
-	seg.build(0, 0, n-1, arr);
-	// for(auto& e:seg.segArr) cout<<e<<" ";cout<<endl;
-    int q;
+
+	parent.resize(n+1);
+	sizeArr.resize(n+1);
+	
+	makeSet(n);
+	
+	int q;
 	cin>>q;
+	int diff = 0;
 	while(q--){
-		int type;
-		cin>>type;
-		if(type==1){
-			// type 1 --> range query
-			int l, h;
-			cin>>l>>h;
-			cout<<seg.rangeQuery(0, 0, n-1, l, h)<<endl;
-		}
-		else if(type==2){
-			// type 2 --> range update
-			int l, h, val;
-			cin>>l>>h>>val;
-			seg.rangeUpdate(0, 0, n-1, l, h, val);
+		int a, b;
+		cin>>a>>b;
+		if(findParent(a) == findParent(b)){
+			cout<<diff<<endl;
+		}else{
+			unionSet(a, b);
+			auto right = sizes.lower_bound(sizeArr[findParent(a)]);
+			diff = min(diff, *right - sizeArr[findParent(a)]);
+			auto left = (right!=sizes.begin()) ? (--right) : (right);
+			diff = min(diff, sizeArr[findParent(a)] - *left);
+			cout<<diff<<endl;
 		}
 	}
-    
     
     return 0;
 }
